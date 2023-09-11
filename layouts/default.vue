2e2fc8ce6f0eef727d593801c8bc0d1dbe4c68c8
Fabian Kirchhoff <fabian.kirchhoff@anny.co>
2023-09-11 22:23:41 +0200

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="slidev-layout default">
+    <slot />
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.slidev-layout.default {
+  background-image: var(--anny-slide);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  padding: 4em 5em;
+}
+</style>
---
+++ layouts/intro.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="slidev-layout intro">
+    <div class="my-auto w-full">
+      <slot />
+    </div>
+  </div>
+</template>
+
+
+<style lang="postcss">
+.slidev-layout.intro {
+  @apply grid;
+
+  h1 {
+    @apply text-6xl leading-20;
+  }
+
+  h1 + p {
+    @apply text-2xl -mt-6;
+  }
+}
+</style>
+<script setup>
+</script>
